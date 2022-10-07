11da8ba76fb380a6e94cf932498807f4ea0d4d70
Yosefe27 <chapotokajoseph@gmail.com>
2022-10-07 14:47:45 +0200

    Facilitator - Adding Group Member (All fields saving and retrivable)

--- app/src/main/java/com/bluecode/weledger/Constants.java
+++ app/src/main/java/com/bluecode/weledger/Constants.java
@@ -5,19 +5,7 @@ package com.bluecode.weledger;
 
 public final class Constants {
 
-
-
-//    /*=======================TEST SERVER URLS================================*/
-    //public static final String BASE_URL = "https://myauthor.delatinotech.com/weeledger/apis/";
     public static final String BASE_URL = "https://microlinksolutionsltd.com/apis/";
     public static final String MIXPANEL_TOKEN = "test";
-
-
-    /*=======================PRODUCTION SERVER URLS================================*/
-//    public static final String BASE_MSSQL_LIVE = "https://portals.zsiclife.co.zm:6094/live_apis/";
-//    public static final String MIXPANEL_TOKEN = "3eaefb51404dd62d783f5da73e4f98ab";
-
-
-
 
 }
