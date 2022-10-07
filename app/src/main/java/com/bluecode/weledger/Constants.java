7a4b0c1571e021bb5f00af6135d01293db27a2fe
Yosefe27 <chapotokajoseph@gmail.com>
2022-10-07 14:55:43 +0200

    Runtime Bug fix

--- app/src/main/java/com/bluecode/weledger/Constants.java
+++ app/src/main/java/com/bluecode/weledger/Constants.java
@@ -8,4 +8,6 @@ public final class Constants {
     public static final String BASE_URL = "https://microlinksolutionsltd.com/apis/";
     public static final String MIXPANEL_TOKEN = "test";
 
+    public static final String GROUP_NAME = "group_name";
+
 }
