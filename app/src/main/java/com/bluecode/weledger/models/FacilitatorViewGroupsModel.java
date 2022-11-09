15b725d4ce456a810a5a68ca832cc8eeb670883b
unknown <josephphiri909@gmail.com>
2022-11-09 14:13:49 +0200

    Fix regrouping of members to a particular group layout and apply validation when a facililitator is adding a new member to a particular group

--- app/src/main/java/com/bluecode/weledger/models/FacilitatorViewGroupsModel.java
+++ app/src/main/java/com/bluecode/weledger/models/FacilitatorViewGroupsModel.java
@@ -4,16 +4,15 @@ public class FacilitatorViewGroupsModel {
     private String
             group_name,
             group_id,
-            member_name,
-            member_id,
-            member_contribution;
+            member_name;
+//            member_id,
+//            member_contribution;
 
-    public FacilitatorViewGroupsModel(String group_name, String group_id, String member_name, String member_id, String member_contribution) {
+
+    public FacilitatorViewGroupsModel(String group_name, String group_id, String member_name) {
         this.group_name = group_name;
         this.group_id = group_id;
         this.member_name = member_name;
-        this.member_id = member_id;
-        this.member_contribution = member_contribution;
     }
 
     public String getGroup_name() {
@@ -38,21 +37,5 @@ public class FacilitatorViewGroupsModel {
 
     public void setMember_name(String member_name) {
         this.member_name = member_name;
-    }
-
-    public String getMember_id() {
-        return member_id;
-    }
-
-    public void setMember_id(String member_id) {
-        this.member_id = member_id;
-    }
-
-    public String getMember_contribution() {
-        return member_contribution;
-    }
-
-    public void setMember_contribution(String member_contribution) {
-        this.member_contribution = member_contribution;
     }
 }
