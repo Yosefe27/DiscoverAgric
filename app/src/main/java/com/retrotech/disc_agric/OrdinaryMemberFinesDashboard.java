681d7dc187993497058d5014e1005c5c0436df89
Yosefe27 <75086635+Yosefe27@users.noreply.github.com>
2024-08-04 12:27:09 +0200

    Modifications
    Modifications commit

---
+++ app/src/main/java/com/retrotech/disc_agric/OrdinaryMemberFinesDashboard.java
@@ -0,0 +1,14 @@
+package com.retrotech.disc_agric;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrdinaryMemberFinesDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordinary_member_fines_dashboard);
+    }
+}
